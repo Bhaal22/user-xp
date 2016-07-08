f26b703eedd8c4642400a26a0481cf1c91728ef7
Bhaal22 <muller.john@gmail.com>
2016-07-08 23:45:26 +0200

    User Xp for Gmail

--- cef/o365/Program.cs
+++ cef/o365/Program.cs
@@ -20,17 +20,30 @@ namespace o365
             //Perform dependency check to make sure all relevant resources are in our output directory.
             Cef.Initialize(settings, shutdownOnProcessExit: true, performDependencyCheck: true);
 
+            var techno = args[0];
+            var url = args[1];
+            var username = args[2];
+            var password = args[3];
 
-            var url = args[0];
-            var username = args[1];
-            var password = args[2];
 
-            var scenario = new SPOnlineScenario(url, username, password);
+            if (techno == "gmail")
+            {
+                var scenario = new GmailScenario(url, username, password);
+                scenario.Run();
 
-            scenario.Run();
+                // We have to wait for something, otherwise the process will exit too soon.
+                scenario.WaitForEndOfScenario();
+            }
+            else if (techno == "sharepoint")
+            {
+                var scenario = new SPOnlineScenario(url, username, password);
+                scenario.Run();
 
-            // We have to wait for something, otherwise the process will exit too soon.
-            scenario.WaitForEndOfScenario();
+                // We have to wait for something, otherwise the process will exit too soon.
+                scenario.WaitForEndOfScenario();
+            }
+            else
+                Console.WriteLine("Unsupported technology");
 
             // Clean up Chromium objects.  You need to call this in your application otherwise
             // you will get a crash when closing.
